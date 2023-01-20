4800b3cae5e671cfebd152043088bb3cc6cbf2dd
fhgaha <antsy1992@gmail.com>
2023-01-20 05:27:00 +0300

    contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,25 @@ import { MdOutlineEmail } from "react-icons/md";
 import { SlSocialVkontakte } from "react-icons/sl";
 import { FaTelegramPlane } from "react-icons/fa";
 
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_llo4qxt', 'template_5xu9bzs', form.current, '-xk5Q1KmyzwiFqdc-')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    
+    e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -13,26 +31,33 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>antsy1992@gmail.com</h5>
-            <a href="mailto:antsy1992@gmail.com" target='_blank'>Send a message</a>
+            <a href="mailto:antsy1992@gmail.com" target='_blank' rel="noreferrer">Send a message</a>
           </article>
 
           <article className="contact__option">
-            <FaTelegramPlane />
+            <FaTelegramPlane className='contact__option-icon' />
             <h4>Telegram</h4>
             <h5>@fhgaha</h5>
             <a href="https://web.telegram.org/" target="@fhgaha">Send a message</a>
           </article>
 
           <article className="contact__option">
-            <SlSocialVkontakte />
+            <SlSocialVkontakte className='contact__option-icon' />
             <h4>VK</h4>
             <h5>id73770321</h5>
             <a href="https://vk.com/" target="id73770321">Send a message</a>
           </article>
         </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder='Your Full Name' required />
+          <input type="email" name="email" placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your message' required></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
